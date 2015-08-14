d04cc8dcd27683c9949be2d26877f4295db4850d
Tomoyuki Sahara <tsahara@iij.ad.jp>
2015-08-14 12:03:07 +0900

    number of arguments is "int", not "mrb_int" (on 1.0.0).

--- src/class.c
+++ src/class.c
@@ -1210,7 +1210,7 @@ mrb_bob_missing(mrb_state *mrb, mrb_value mod)
 {
   mrb_sym name;
   mrb_value *a;
-  mrb_int alen;
+  int alen;
 
   mrb_get_args(mrb, "n*", &name, &a, &alen);
   mrb_method_missing(mrb, name, mod, mrb_ary_new_from_values(mrb, alen, a));
