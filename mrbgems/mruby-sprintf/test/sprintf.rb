c0b5ea34a012da1ccf5466cbcdf6ad5d22e2e825
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2015-11-19 02:16:47 +0900

    binary sprintf should not be restricted by mrb_int size; fix #3025

--- mrbgems/mruby-sprintf/test/sprintf.rb
+++ mrbgems/mruby-sprintf/test/sprintf.rb
@@ -1,3 +1,9 @@
 ##
 # Kernel#sprintf Kernel#format Test
 
+assert('String#%') do
+  assert_equal "one=1", sprintf("one=%d", 1)
+  assert_equal "1 one 1.0", sprintf("%d %s %3.1f", 1, "one", 1.01)
+  assert_equal "123 < 456", sprintf("%{num} < %<str>s", { num: 123, str: "456" })
+  assert_equal 16, sprintf("%b", (1<<15)).size
+end
